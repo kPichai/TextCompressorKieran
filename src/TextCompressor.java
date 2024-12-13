a91349de8e2a31f7c607bf7dd20c2673878e5227
kpi <kieran.pichai@menloschool.org>
2024-12-13 00:42:38 -0800

    asdasd

--- src/TextCompressor.java
+++ src/TextCompressor.java
@@ -28,61 +28,85 @@
  *  @author Zach Blick, Kieran Pichai
  */
 public class TextCompressor {
+    // Length of codes for our compression and expansion, 12 as default
     final private static int CODE_LENGTH = 12;
 
+    // Compress algorithm reads in a string and compresses it using LZW compression
     private static void compress() {
+        // Reads in string and initialies TST used for storing all codes
         String text = BinaryStdIn.readString();
         TST codes = new TST();
         int codesLen = (int)Math.pow(2, CODE_LENGTH);
+        // Assigns all initial values in the TST as well as our starting code
         for (int i = 0; i < 256; i++) {
             codes.insert("" + (char)i, i);
         }
         int startCode = 257;
         String prefix;
+        // Loops through our text
         for (int i = 0; i < text.length();) {
+            // Reads in prefix and its associated code
             prefix = codes.getLongestPrefix(text, i);
             int associatedCode = codes.lookup(prefix);
+            // Writes the code to the output file
             BinaryStdOut.write(associatedCode, CODE_LENGTH);
-            System.err.println("Writing: " + (associatedCode));
+            // Printing statements for testing
+//            System.err.println("Writing: " + (associatedCode));
+            // Checks to see we aren't going out of bounds or have run out of codes
             if (i + prefix.length() < text.length() && startCode < codesLen) {
                 char next = text.charAt(i + prefix.length());
+                // Inserts our next code into the TST for future lookups
                 codes.insert(prefix + next, startCode);
-                System.err.println("Adding Code: " + (prefix + next) + " Code: " + startCode);
+//                System.err.println("Adding Code: " + (prefix + next) + " Code: " + startCode);
                 startCode++;
             }
+            // Increments our spot in the text string according to how much we wrote out in our prefix
             i += prefix.length();
         }
+        // Writes EOF and closes file
         BinaryStdOut.write(256, CODE_LENGTH);
         BinaryStdOut.close();
     }
 
+    // Expands code via a compressed txt file
     private static void expand() {
+        // Map used to access codes and their corresponding string values
         String[] codes = new String[(int) Math.pow(2, CODE_LENGTH)];
         int codesLen = (int) Math.pow(2, CODE_LENGTH);
+        // Fills it with known values (ASCII)
         for (int i = 0; i <= 256; i++) {
             codes[i] = "" + (char) i;
             if (i == 256) {
                 codes[i] = "";
             }
         }
+        // Reads in and initializes all necessary values to expand based off of current and future values
         int maxCode = 257;
         int valCode = BinaryStdIn.readInt(CODE_LENGTH);
         String val = codes[valCode];
         BinaryStdOut.write(val);
         int nextCode;
+        // Infinite loop to be broken when EOF is seen
         while (true) {
+            // Checks next code to see if we will exit then
             nextCode = BinaryStdIn.readInt(CODE_LENGTH);
-            if (nextCode == 256) break;
+            if (nextCode == 256) {
+                break;
+            }
+            // Makes our entry string which represents what we will write out to our BinaryStdOut file
             String entry;
             if (nextCode < maxCode && codes[nextCode] != null) {
                 entry = codes[nextCode];
             } else {
+                // Special case where we don't know where our current code to add, use know val instead
                 entry = val + val.charAt(0);
             }
             BinaryStdOut.write(entry);
+            // Checks if space left and if so adds more to codes
             if (maxCode < codesLen) {
                 codes[maxCode++] = val + entry.charAt(0);
             }
+            // Continues along codes by moving to next
             val = entry;
         }
         BinaryStdOut.close();
